1dead3c894a4a3bf27ba9cfde9bffcd61855ef89
trixtertop <gadelovevgenij4@gmail.com>
2023-01-11 18:05:52 +0600

    create to do list

--- assets/js/lesson-6.js
+++ assets/js/lesson-6.js
@@ -1,12 +1,13 @@
 document.querySelector("#btn-add").addEventListener("click", createNewToDo);
 const toDoText = document.querySelector("#new-task");
 const todoWorkspace = document.querySelectorAll(".todo__list");
-const task = document.createElement("div");
-task.classList.add("todo__task");
+
 
 let arrTask = [];
 
 function addTask(name) {
+  const task = document.createElement("div");
+  task.classList.add("todo__task");
   todoWorkspace[0].append(task);
   const nameTask = document.createElement("p");
   nameTask.classList.add("todo__name");
@@ -17,7 +18,28 @@ function addTask(name) {
   task.append(nameTask);
   nameTask.append(closeTask);
   arrTask.push(toDoText.value);
+  const progress = document.createElement("button");
+  progress.textContent = "Add in progress";
+  progress.classList.add("btn-in-progress");
+  progress.addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn-in-progress")) {
+      const task = document.querySelector(".todo__task")
+      task.remove();
+      todoWorkspace[1].append(task);
+      progress.textContent = "Done";
+    }
+  });
+  task.append(progress);
 }
+
+// progress.addEventListener("click", function (event) {
+//   if (event.target.classList.contains("btn-in-progress")) {
+//     const task = document.querySelector(".todo__task")
+//     task.remove();
+//     todoWorkspace[1].append(task);
+//     progress.textContent = "Done";
+//   }
+// });
 
 function createNewToDo() {
   addTask(toDoText.value);
@@ -25,13 +47,13 @@ function createNewToDo() {
   localStorage.setItem("task", JSON.stringify(arrTask) || []);
 }
 
-function itemRemove(i) {
-  const removeArr = JSON.parse(localStorage.getItem("task") || []);
-  if (removeArr) {
-    removeArr.splice(i, 1);
-    localStorage.setItem("task", JSON.stringify(removeArr));
-  }
-}
+// function itemRemove(i) {
+//   const removeArr = JSON.parse(localStorage.getItem("task") || []);
+//   if (removeArr) {
+//     removeArr.splice(i, 1);
+//     localStorage.setItem("task", JSON.stringify(removeArr));
+//   }
+// }
 
 function init() {
   arrTask = JSON.parse(localStorage.getItem("task") || []);
